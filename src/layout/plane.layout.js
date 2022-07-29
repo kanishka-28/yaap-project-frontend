d88fabf23031d47b901ea5b18202132dc76ca97b
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2022-07-29 09:53:02 +0530

    app.js modified

--- src/layout/plane.layout.js
+++ src/layout/plane.layout.js
@@ -8,12 +8,12 @@ import yellowCircle from "../assets/images/yellowCircle.svg"
 const PlaneLayout = (props) => {
     return (
         <div className='h-screen sm:flex gap-96'>
-            <div className='hidden sm:block'>
+            <div className=''>
                 <Link to="/">
                     <img src={logoyaap} alt="home" className="relative ml-24 top-16" />
                 </Link>
                 <img src={quarterCircle} alt="home" className="h-96 relative bottom-10" />
-                <img src={yellowCircle} alt="home" className="relative top-32 ml-20" />
+                <img src={yellowCircle} alt="home" className="sm:relative sm:top-32 sm:ml-20" />
             </div>
             <div className=''>
                 {props.children}
