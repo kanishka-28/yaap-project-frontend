c5e7a3dad8d49e82c0049debf496f7c9e52fc3b4
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2022-07-30 12:03:31 +0530

    redirect link updated

--- src/Pages/information/thanks.js
+++ src/Pages/information/thanks.js
@@ -4,16 +4,18 @@ import PlaneLayout from '../../layout/plane.layout'
 const Thanks = () => {
 
     useEffect(() => {
-      setTimeout(() => {
-        window.location.href = "https://spatial.io/s/Yaap-Main-Venue-62d717a487de560001106ab9";
-      }, 3000);      
-    }, []);    
+        setTimeout(() => {
+            window.location.href = "https://spatial.io/s/Yaap-Main-Venue-62d717a487de560001106ab9";
+        }, 3000);
+    }, []);
 
     return (
         <PlaneLayout>
             <div className='md:mt-56 flex flex-col m-3'>
                 <h2 className='font-bold text-4xl'>Thank you!</h2>
-                <p className='text-xs mt-4 text-gray-600'>You will now be redirected to our event page. <span className='text-yellow-600'>Click here </span>if you are still not redirected</p>
+                <p className='text-xs mt-4 text-gray-600'>You will now be redirected to our event page. <span className='cursor-pointer text-yellow-600' onClick={() => {
+                    window.location.href = "https://spatial.io/s/Yaap-Main-Venue-62d717a487de560001106ab9";
+                }}>Click here </span>if you are still not redirected</p>
             </div>
         </PlaneLayout>
     )
