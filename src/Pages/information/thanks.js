24f85937ebcd641a7c05e4c76c7097f416490fa2
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2022-07-30 11:53:04 +0530

    redirect link added

--- src/Pages/information/thanks.js
+++ src/Pages/information/thanks.js
@@ -7,8 +7,7 @@ const Thanks = () => {
       setTimeout(() => {
         window.location.href = "https://spatial.io/s/Yaap-Main-Venue-62d717a487de560001106ab9";
       }, 3000);      
-    }, []);
-    
+    }, []);    
 
     return (
         <PlaneLayout>
